f85401ba84dff9868f2c611c5e51b520535ffc93
JTyzz <jtyzzer@gmail.com>
2019-04-24 17:09:44 -0700

    Added POST method for adding User data via json.

--- app/src/main/java/com/example/labs12_wellness_bet_sleep_android/Models/User.java
+++ app/src/main/java/com/example/labs12_wellness_bet_sleep_android/Models/User.java
@@ -3,8 +3,10 @@ package com.example.labs12_wellness_bet_sleep_android.Models;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Timestamp;
+
 public class User {
-    private String password, username;
+    private String password, username, email, role, photoUrl;
 
     public User(){
 
@@ -12,12 +14,27 @@ public class User {
 
     public User(JSONObject json){
         try {
-            this.username = json.getString("username");
+            this.username = json.getString("full_name");
         } catch (JSONException e) {
             e.printStackTrace();
         }
         try {
             this.password = json.getString("password");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.email = json.getString("email");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.role = json.getString("role");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.role = json.getString("photoUrl");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -37,5 +54,29 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
     }
 }
--- app/src/main/java/com/example/labs12_wellness_bet_sleep_android/SignUp/CreateAccount.java
+++ app/src/main/java/com/example/labs12_wellness_bet_sleep_android/SignUp/CreateAccount.java
@@ -3,22 +3,43 @@ package com.example.labs12_wellness_bet_sleep_android.SignUp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.labs12_wellness_bet_sleep_android.Network.NetworkAdapter;
 import com.example.labs12_wellness_bet_sleep_android.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class CreateAccount extends AppCompatActivity {
+    
+    public static final String TAG = "CreateAccountTag";
 
     TextView login, termOfS;
+    EditText nameText, emailText, passwordText;
+    CardView signupButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        login = (TextView) findViewById(R.id.textView_logIn);
-        termOfS = (TextView) findViewById(R.id.textView_terms);
+        login = findViewById(R.id.textView_logIn);
+        termOfS = findViewById(R.id.textView_terms);
+
+        nameText = findViewById(R.id.name_text_ca);
+        passwordText = findViewById(R.id.password_text_ca);
+        emailText = findViewById(R.id.email_text_ca);
+
+        signupButton = findViewById(R.id.cardView_signup);
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +47,35 @@ public class CreateAccount extends AppCompatActivity {
                 Intent MainActivityIntent = new Intent(CreateAccount.this, LogInActivity.class);
                 startActivity(MainActivityIntent);
 
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        JSONObject userdata = new JSONObject();
+                        try {
+                            userdata.put("full_name", nameText.getText().toString());
+                            userdata.put("password", passwordText.getText().toString());
+                            userdata.put("email", emailText.getText().toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        String tokenRequest = null;
+                        try {
+                            tokenRequest = NetworkAdapter.httpRequest(
+                                    "http://10.0.2.2:1932/product",
+                                    "POST", userdata, null);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        Log.i(TAG, tokenRequest);
+                    }
+                }).start();
             }
         });
 
