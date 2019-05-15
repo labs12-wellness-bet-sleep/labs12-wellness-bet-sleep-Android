4334c44d8778113c8201e7386b1509b418e00287
Pablo Ogata <pabloogata@gmail.com>
2019-05-15 19:13:19 -0400

    temp

--- app/src/main/java/com/example/labs12_wellness_bet_sleep_android/fragmentsNav/roomsFragment.java
+++ app/src/main/java/com/example/labs12_wellness_bet_sleep_android/fragmentsNav/roomsFragment.java
@@ -1,5 +1,6 @@
 package com.example.labs12_wellness_bet_sleep_android.fragmentsNav;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.annotation.NonNull;
@@ -8,14 +9,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.labs12_wellness_bet_sleep_android.R;
+import com.example.labs12_wellness_bet_sleep_android.fragmentsNav.innerActivity.joinCode;
 
 public class roomsFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_create_join, container, false);
+        View view =  inflater.inflate(R.layout.fragment_create_join, container, false);
+
+        ImageView joinGroup = (ImageView)view.findViewById(R.id.imageView_join);
+
+        joinGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent in = new Intent(getActivity().getBaseContext(), joinCode.class);
+                in.putExtra("some", "some data");
+                getActivity().startActivity(in);
+
+
+
+            }
+        });
+
+
+
+
+
+        return view;
+
     }
 }
