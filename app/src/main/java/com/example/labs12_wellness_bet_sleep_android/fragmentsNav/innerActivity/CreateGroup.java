9555eaf689637d5dfde4cbdf82a607a220fffdbe
Pablo Ogata <pabloogata@gmail.com>
2019-05-16 21:37:46 -0400

    group xml

---
+++ app/src/main/java/com/example/labs12_wellness_bet_sleep_android/fragmentsNav/innerActivity/CreateGroup.java
@@ -0,0 +1,13 @@
+package com.example.labs12_wellness_bet_sleep_android.fragmentsNav.innerActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+}
